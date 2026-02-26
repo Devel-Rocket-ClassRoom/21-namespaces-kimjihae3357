da03bd175b33a15d6f8042c86966932f9d1da10f
bmj1644035 <bmj1644035@gmail.com>
2026-02-26 18:05:13 +0900

    21챕터 과제제출

--- GameSystem/Program.cs
+++ GameSystem/Program.cs
@@ -1,4 +1,60 @@
 using System;
+using MyGame.Characters;
+using MyGame.Items;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+MyGame.Characters.Player character = new MyGame.Characters.Player();
+character.Attack();
+MyGame.Characters.Enemy character = new MyGame.Characters.Enemy();
+character.TakeDamage();
+
+MyGame.Items.Weapon item = new MyGame.Items.Weapon();
+item.GetDamage();
+
+
+namespace MyGame.Characters
+{
+    public class Player
+    {
+        public string Name = "용사";
+
+        public void Attack()
+        {
+            Console.WriteLine($"플레이어 {Name}(이)가 공격합니다!");
+        }
+    }
+
+    public class Enemy
+    {
+        public string Type = "고블린";
+
+        public void TakeDamage()
+        {
+            Console.WriteLine($"적 {Type}(이)가 데미지를 받았습니다!");
+        }
+    }
+}
+
+namespace MyGame.Items
+{
+    public class Weapon
+    {
+        public string Name = "불꽃검";
+        public int attack = 50;
+
+        public void GetDamage()
+        {
+            Console.WriteLine($"무기 {Name}의 공격력: {attack}");
+        }
+    }
+
+    public class Potion
+    {
+        public string Name = "체력 포션";
+
+        public void Use()
+        {
+            Console.WriteLine($"포션 {Name}을(를) 사용했습니다.");
+        }
+    }
+
+}
